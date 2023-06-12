c2b821a0995c952fd9c0ecd18fec77d8744c7e80
Matt Seng <mmseng@users.noreply.github.com>
2023-06-12 14:24:19 -0500

    Update Set-RemoteService.psm1

--- Set-RemoteService.psm1
+++ Set-RemoteService.psm1
@@ -20,7 +20,8 @@ function Set-RemoteService {
 	function log {
 		param(
 			[string]$Msg,
-			[int]$L
+			[int]$L,
+			[switch]$NoNewLine
 		)
 		
 		$indent = ""
@@ -32,7 +33,14 @@ function Set-RemoteService {
 		
 		$msg = "[$ts]$indent $msg"
 		
-		Write-Host $msg
+		$params = @{
+			Object = $msg
+		}
+		if($NoNewLine) {
+			$params.NoNewLine = $true
+		}
+		
+		Write-Host @params
 	}
 	
 	function Get-Comps {
@@ -108,8 +116,44 @@ function Set-RemoteService {
 		}
 	}
 	
+	function Confirm-Continue {
+		log "Confirming intention..."
+		
+		if($Status -or $StartType) {
+			if($Confirm) {
+				log "-Confirm was specified. Continuing." -L 1
+			}
+			else {
+				log "Desired state:" -L 1
+				log "Status: `"$Status`"" -L 2
+				log "StartType: `"$StartType`"" -L 2
+				log "Are you sure you want to continue? Enter 'Y' to confirm" -L 1 -NoNewLine
+				$input = Read-Host
+				if($input.ToLower() -eq "y") {
+					$confirmed = $true
+					log "User confirmed. Continuing."
+				}
+				else {
+					log "User did not confirm. Aborting."
+				}
+			}
+		}
+		else {
+			log "-Status and -StartType are both unspecified. No changes will be made."
+			$confirmed = $true
+		}
+		
+		$confirmed
+	}
+	
 	function Do-Stuff {
 		$comps = Get-Comps
-		$states = Set-ServiceOnComps $comps
+		if(Confirm-Continue) {
+			$states = Set-ServiceOnComps $comps
+		}
 	}
+	
+	Do-Stuff
+	
+	log "EOF"
 }
